b5d89b522c8393ac7a96cc0963537fb8f4d70712
JhonRamosV <86128581+JhonRamosV@users.noreply.github.com>
2021-08-17 08:55:34 -0500

    crut del las primeras tablas

---
+++ proyecto3trimestresena/Controllers/ProveedorController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using proyecto3trimestresena.Models;
+namespace proyecto3trimestresena.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        // GET: Proveedor
+        public ActionResult Index()
+        {
+
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.proveedor.ToList());
+            }
+
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+
+                using (var db = new inventario2021Entities())
+                {
+                    db.proveedor.Add(proveedor);
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                var findUser = db.proveedor.Find(id);
+                return View(findUser);
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+
+                using (var bd = new inventario2021Entities())
+                {
+                    var findUser = bd.proveedor.Find(id);
+
+                    bd.proveedor.Remove(findUser);
+                    bd.SaveChanges();
+                    return RedirectToAction("index");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+
+            }
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var bd = new inventario2021Entities())
+                {
+                    proveedor findUser = bd.proveedor.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+
+
+
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(proveedor editUser)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    proveedor user = db.proveedor.Find(editUser.id);
+
+                    user.nombre = editUser.nombre;
+                    user.direccion = editUser.direccion;
+                    user.telefono = editUser.telefono;
+                    user.nombre_contacto = editUser.nombre_contacto;
+
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+    }
+}
