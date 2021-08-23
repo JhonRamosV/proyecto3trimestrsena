e27fc8ac5d2c60064522579973ffa628af4d7f86
JhonRamosV <86128581+JhonRamosV@users.noreply.github.com>
2021-08-22 20:04:11 -0500

    primer crud de tabla con llave foranea

---
+++ proyecto3trimestresena/Controllers/ProductoController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using proyecto3trimestresena.Models;
+namespace proyecto3trimestresena.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.producto.ToList());
+            }
+        }
+
+
+
+        public static string NombreProveedor(int idProveedor)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.proveedor.Find(idProveedor).nombre;
+            }
+        }
+
+
+
+        public ActionResult ListarProveedores()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.proveedor.ToList());
+            }
+        }
+
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(producto producto)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.producto.Add(producto);
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.producto.Find(id));
+            }
+        }
+
+
+        public ActionResult Edit(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                producto productoEdit = db.producto.Where(a => a.id == id).FirstOrDefault();
+                return View(productoEdit);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(producto productoEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var oldproducto = db.producto.Find(productoEdit.id);
+                    oldproducto.nombre = productoEdit.nombre;
+                    oldproducto.cantidad = productoEdit.cantidad;
+                    oldproducto.descripcion = productoEdit.descripcion;
+                    oldproducto.percio_unitario = productoEdit.percio_unitario;
+                    oldproducto.id_proveedor = productoEdit.id_proveedor;
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+
+
+
+
+
+
+
+
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    producto producto = db.producto.Find(id);
+                    db.producto.Remove(producto);
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+                }
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+
+            }
+        }
+    
+    }
+}
