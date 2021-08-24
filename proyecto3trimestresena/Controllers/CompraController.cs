e42ebb0c9f404178e80eb51d5acaf29505933365
JhonRamosV <86128581+JhonRamosV@users.noreply.github.com>
2021-08-23 19:07:22 -0500

    crud de la tabla compra

---
+++ proyecto3trimestresena/Controllers/CompraController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using proyecto3trimestresena.Models;
+
+namespace proyecto3trimestresena.Controllers
+{
+    public class CompraController : Controller
+    {
+        // GET: Compra
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.compra.ToList());
+            }
+        }
+
+
+        public static string NombreUsuario(int id_Usuario)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.usuario.Find(id_Usuario).nombre;
+            }
+        }
+
+        public ActionResult ListarUsuarioos()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.usuario.ToList());
+            }
+        }
+
+        public static string NombreCliente(int id_Cliente)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return db.cliente.Find(id_Cliente).nombre;
+            }
+        }
+
+        public ActionResult ListarCliente()
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return PartialView(db.cliente.ToList());
+            }
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+
+        public ActionResult Create(compra compra)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.compra.Add(compra);
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                return View(db.compra.Find(id));
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            using (var db = new inventario2021Entities())
+            {
+                compra compraEdit = db.compra.Where(a => a.id == id).FirstOrDefault();
+                return View(compraEdit);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Edit(compra compraEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    var olducompra = db.compra.Find(compraEdit.id);
+                    olducompra.fecha = compraEdit.fecha;
+                    olducompra.total = compraEdit.total;
+                    olducompra.id_usuario = compraEdit.id_usuario;
+                    olducompra.id_cliente = compraEdit.id_cliente;
+
+
+
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+
+
+
+
+
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    compra compra = db.compra.Find(id);
+                    db.compra.Remove(compra);
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+
+            }
+        }
+    }
+}
