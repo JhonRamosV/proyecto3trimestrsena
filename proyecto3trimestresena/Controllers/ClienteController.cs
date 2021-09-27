ce236078c1d64969b6aaa8cef64f500d72e8083b
JhonRamosV <86128581+JhonRamosV@users.noreply.github.com>
2021-09-27 17:34:43 -0500

    csv de cliente y proveedor

--- proyecto3trimestresena/Controllers/ClienteController.cs
+++ proyecto3trimestresena/Controllers/ClienteController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using proyecto3trimestresena.Models;
+using System.IO;
 
 namespace proyecto3trimestresena.Controllers
 {
@@ -138,8 +139,102 @@ namespace proyecto3trimestresena.Controllers
                 ModelState.AddModelError("", "error" + ex);
                 return View();
             }
+
+
+
+          
+        }
+        public ActionResult Edit(proveedor editUser)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    proveedor user = db.proveedor.Find(editUser.id);
+
+                    user.nombre = editUser.nombre;
+                    user.direccion = editUser.direccion;
+                    user.telefono = editUser.telefono;
+                    user.nombre_contacto = editUser.nombre_contacto;
+
+                    db.SaveChanges();
+                    return RedirectToAction("index");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
         }
 
 
-    }
+
+        public ActionResult uploadCSV()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+
+        public ActionResult uploadCSV(HttpPostedFileBase fileForm)
+        {
+            try
+            {
+
+                string filePath = string.Empty;
+                if (fileForm != null)
+                {
+                    string path = Server.MapPath("~/Uploads/");
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    filePath = path + Path.GetFileName(fileForm.FileName);
+
+                    string extension = Path.GetExtension(fileForm.FileName);
+
+                    fileForm.SaveAs(filePath);
+
+                    string csvData = System.IO.File.ReadAllText(filePath);
+
+                    foreach (string row in csvData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            var newCliente = new cliente
+                            {
+                                nombre = row.Split(';')[0],
+                                documento = row.Split(';')[1],
+                                email = row.Split(';')[2],
+                              
+                            };
+                            using (var db = new inventario2021Entities())
+                            {
+                                db.cliente.Add(newCliente);
+                                db.SaveChanges();
+                            }
+                        }
+                    }
+
+
+                }
+
+                return View();
+
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+
+        }
+        
 }
--- proyecto3trimestresena/Controllers/ProveedorController.cs
+++ proyecto3trimestresena/Controllers/ProveedorController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using proyecto3trimestresena.Models;
+using System.IO;
 namespace proyecto3trimestresena.Controllers
 {
     
@@ -129,6 +130,71 @@ namespace proyecto3trimestresena.Controllers
                 }
             }
             catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+
+
+        public ActionResult uploadCSV()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+
+        public ActionResult uploadCSV(HttpPostedFileBase fileForm)
+        {
+            try
+            {
+
+                string filePath = string.Empty;
+                if(fileForm != null)
+                {
+                    string path = Server.MapPath("~/Uploads/");
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    filePath = path + Path.GetFileName(fileForm.FileName);
+
+                    string extension = Path.GetExtension(fileForm.FileName);
+
+                    fileForm.SaveAs(filePath);
+
+                    string csvData = System.IO.File.ReadAllText(filePath);
+
+                    foreach(string row in csvData.Split('\n'))
+                    {
+                        if (!string.IsNullOrEmpty(row))
+                        {
+                            var newProveedor = new proveedor
+                            {
+                                nombre = row.Split(';')[0],
+                                direccion = row.Split(';')[1],
+                                telefono = row.Split(';')[2],
+                                nombre_contacto = row.Split(';')[3]
+                            };
+                            using (var db = new inventario2021Entities())
+                            {
+                                db.proveedor.Add(newProveedor);
+                                db.SaveChanges();
+                            }
+                        }
+                    }
+
+               
+                }
+
+                return View();
+
+
+            }
+            catch(Exception ex)
             {
                 ModelState.AddModelError("", "error" + ex);
                 return View();
