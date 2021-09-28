7afefd04b223e627e24a14c2c41d05ca934e33b8
JhonRamosV <86128581+JhonRamosV@users.noreply.github.com>
2021-09-27 19:23:03 -0500

    cargue de imagenes

--- proyecto3trimestresena/Controllers/Producto_imagenController.cs
+++ proyecto3trimestresena/Controllers/Producto_imagenController.cs
@@ -4,12 +4,13 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using proyecto3trimestresena.Models;
+using System.IO;
 
 namespace proyecto3trimestresena.Controllers
 {
     public class Producto_imagenController : Controller
     {
-        
+
         // GET: Producto_imagen
         public ActionResult Index()
         {
@@ -134,6 +135,75 @@ namespace proyecto3trimestresena.Controllers
                 return View();
 
             }
+
+
+
+
+
         }
+
+
+        public ActionResult CargarImagen()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CargarImagen(int producto, HttpPostedFileBase imagen)
+        {
+            try
+            {
+                //string para guardar la ruta
+                string filePath = string.Empty;
+                string nameFile = "";
+
+                //condicion para saber si el archivo llego
+                if (imagen != null)
+                {
+                    //ruta de la carpeta que guardara el archivo
+                    string path = Server.MapPath("~/Uploads/Imagenes/");
+
+                    //condicion para saber si la carpeta uploads existe
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+
+                    nameFile = Path.GetFileName(imagen.FileName);
+
+                    //obtener el nombre del archivo
+                    filePath = path + Path.GetFileName(imagen.FileName);
+
+                    //obtener la extension del archivo
+                    string extension = Path.GetExtension(imagen.FileName);
+
+                    //guardar el archivo
+                    imagen.SaveAs(filePath);
+                }
+
+                using (var db = new inventario2021Entities())
+                {
+                    var imagenProducto = new producto_imagen();
+                    imagenProducto.id_producto = producto;
+                    imagenProducto.imagen = "/Uploads/Imagenes/" + nameFile;
+                    db.producto_imagen.Add(imagenProducto);
+                    db.SaveChanges();
+
+                }
+
+                return View();
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+
+        }
+
     }
 }
+
+
--- proyecto3trimestresena/Controllers/ProveedorController.cs
+++ proyecto3trimestresena/Controllers/ProveedorController.cs
@@ -138,47 +138,57 @@ namespace proyecto3trimestresena.Controllers
 
 
 
+      
+
         public ActionResult uploadCSV()
         {
             return View();
         }
 
-
         [HttpPost]
-
         public ActionResult uploadCSV(HttpPostedFileBase fileForm)
         {
             try
             {
 
+                //string para guardar la ruta
                 string filePath = string.Empty;
-                if(fileForm != null)
+
+                //condicion para saber si el archivo llego
+                if (fileForm != null)
                 {
+                    //ruta de la carpeta que guardara el archivo
                     string path = Server.MapPath("~/Uploads/");
+
+                    //condicion para saber si la carpeta uploads existe
                     if (!Directory.Exists(path))
                     {
                         Directory.CreateDirectory(path);
                     }
 
+                    //obtener el nombre del archivo
                     filePath = path + Path.GetFileName(fileForm.FileName);
 
+                    //obtener la extension del archivo
                     string extension = Path.GetExtension(fileForm.FileName);
 
+                    //guardar el archivo
                     fileForm.SaveAs(filePath);
 
                     string csvData = System.IO.File.ReadAllText(filePath);
 
-                    foreach(string row in csvData.Split('\n'))
+                    foreach (string row in csvData.Split('\n'))
                     {
                         if (!string.IsNullOrEmpty(row))
                         {
                             var newProveedor = new proveedor
                             {
                                 nombre = row.Split(';')[0],
-                                direccion = row.Split(';')[1],
                                 telefono = row.Split(';')[2],
+                                direccion = row.Split(';')[1],
                                 nombre_contacto = row.Split(';')[3]
                             };
+
                             using (var db = new inventario2021Entities())
                             {
                                 db.proveedor.Add(newProveedor);
@@ -186,17 +196,14 @@ namespace proyecto3trimestresena.Controllers
                             }
                         }
                     }
-
-               
                 }
 
                 return View();
 
-
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                ModelState.AddModelError("", "error" + ex);
+                ModelState.AddModelError("", "error " + ex);
                 return View();
             }
         }
